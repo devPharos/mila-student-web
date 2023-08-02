7a02e228c06c1da0b0284c4baa13c8dbfb1646be
taugitirana <thayna@pharosit.com.br>
2023-08-02 17:33:36 -0300

    chore: add next-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Image } from '@nextui-org/react'
+
 export default function Home() {
   return (
-    <div className="bg-warning">
-      <h1>mila</h1>
+    <div className="grid grid-columns-2">
+      <div className="bg-primary" />
+      <div className="bg-secondary">
+        <Image />
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 
-import { milaColors } from "./colors"
+import { milaColors } from './colors'
+
+import { nextui } from '@nextui-org/react'
 
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        "primary-dark": milaColors.blue[400],
-        "primary": milaColors.blue[300],
-        "primary-light": milaColors.blue[100], 
-        "secondary-dark": milaColors.red[400],
-        "secondary": milaColors.red[300],
-        "secondary-light": milaColors.red[100],
-        "secondary-lighter": milaColors.red[50],
-        "neutral-darkest": milaColors.gray[400],
-        "neutral-dark": milaColors.gray[300],
-        "neutral": milaColors.gray[200],
-        "neutral-light": milaColors.gray[100],
-        "neutral-lighter": milaColors.gray[50],
-        "information-dark": milaColors.blue[500],
-        "information": milaColors.blue[300],
-        "information-light": milaColors.blue[50],
-        "success-dark": milaColors.green[500],
-        "success": milaColors.green[300],
-        "success-light": milaColors.green[50],
-        "warning-dark": milaColors.orange[500],
-        "warning": milaColors.orange[300],
-        "warning-light": milaColors.orange[50],
-        "error-dark": milaColors.red[500],
-        "error": milaColors.red[300],
-        "error-light": milaColors.red[50],
-      }
+        'primary-dark': milaColors.blue[400],
+        primary: milaColors.blue[300],
+        'primary-light': milaColors.blue[100],
+        'secondary-dark': milaColors.red[400],
+        secondary: milaColors.red[300],
+        'secondary-light': milaColors.red[100],
+        'secondary-lighter': milaColors.red[50],
+        'neutral-darkest': milaColors.gray[400],
+        'neutral-dark': milaColors.gray[300],
+        neutral: milaColors.gray[200],
+        'neutral-light': milaColors.gray[100],
+        'neutral-lighter': milaColors.gray[50],
+        'information-dark': milaColors.blue[500],
+        information: milaColors.blue[300],
+        'information-light': milaColors.blue[50],
+        'success-dark': milaColors.green[500],
+        success: milaColors.green[300],
+        'success-light': milaColors.green[50],
+        'warning-dark': milaColors.orange[500],
+        warning: milaColors.orange[300],
+        'warning-light': milaColors.orange[50],
+        'error-dark': milaColors.red[500],
+        error: milaColors.red[300],
+        'error-light': milaColors.red[50],
+      },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 }
