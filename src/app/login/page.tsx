a71e4da9fb3525fd3fb08cbc529211536b7dee9f
taugitirana <thayna@pharosit.com.br>
2023-08-04 11:13:32 -0300

    feat: create email validation with zod

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,14 +4,38 @@ import { Fingerprint, AtSign, Lock, LogIn } from 'lucide-react'
 import { Image, Input, Button } from '@nextui-org/react'
 import logo from '../assets/logo.png'
 import Link from 'next/link'
+import { z } from 'zod'
+import { useState } from 'react'
 
 export default function Login() {
+  const [value, setValue] = useState<string | undefined>('')
+  const [validateEmail, setValidateEmail] = useState<
+    'invalid' | 'valid' | undefined
+  >(undefined)
+
+  const emailSchema = z.string().email()
+
+  function handleEmailValidation() {
+    try {
+      emailSchema.parse(value)
+      setValidateEmail('valid')
+    } catch (error) {
+      setValidateEmail('invalid')
+    }
+  }
+
   return (
     <div className="grid grid-cols-2 min-h-screen">
       <div className="bg-primary"></div>
       <div className="bg-neutral-lighter flex flex-col items-center justify-center p-6">
         <div className="max-w-sm w-full flex flex-col items-center gap-14">
-          <Image width={130} src={logo.src} alt="" loading="lazy" />
+          <Image
+            width={130}
+            src={logo.src}
+            alt=""
+            loading="lazy"
+            radius="none"
+          />
 
           <div className="flex flex-col gap-6 w-full">
             <Input
@@ -32,13 +56,31 @@ export default function Login() {
 
             <Input
               label="Email"
+              type="email"
               isRequired
               classNames={{
                 label: 'text-neutral',
-                input: ['bg-white', 'text-neutral-dark'],
+                input: [
+                  'bg-white',
+                  validateEmail === 'invalid'
+                    ? 'text-error'
+                    : 'text-neutral-dark',
+                ],
               }}
               startContent={
-                <AtSign className="text-neutral" strokeWidth={1.5} size={20} />
+                <AtSign
+                  className={
+                    validateEmail === 'invalid' ? 'text-error' : 'text-neutral'
+                  }
+                  strokeWidth={1.5}
+                  size={20}
+                />
+              }
+              value={value}
+              onValueChange={setValue}
+              validationState={validateEmail}
+              errorMessage={
+                validateEmail === 'invalid' && 'Please enter a valid email'
               }
             />
 
@@ -60,6 +102,7 @@ export default function Login() {
             className="bg-primary text-white w-full"
             endContent={<LogIn size={20} />}
             radius="md"
+            onClick={handleEmailValidation}
           >
             Log In
           </Button>
