429400bec370758ae24344cea364e808208de6c2
taugitirana <thayna@pharosit.com.br>
2023-08-07 16:38:15 -0300

    refactor: modify step component

--- src/app/components/steps.tsx
+++ src/app/components/steps.tsx
@@ -2,13 +2,14 @@ export function Steps() {
   return (
     <div className="flex items-center justify-center gap-4 w-full">
       <div className="relative">
-        <div className="border-2 border-primary h-10 w-10 rounded-full"></div>
-        <div className="absolute top-4 left-[15px] bg-primary h-2.5 w-2.5 rounded-full"></div>
+        <div className="border-1 border-primary h-8 w-8 rounded-full flex flex-row justify-center items-center">
+          <div className="bg-primary h-2 w-2 rounded-full"></div>
+        </div>
       </div>
-      <div className="bg-white h-1 w-[130px]"></div>
-      <div className="bg-primary h-2.5 w-2.5 rounded-full"></div>
-      <div className="bg-white h-1 w-[130px]"></div>
-      <div className="bg-primary h-2.5 w-2.5 rounded-full"></div>
+      <div className="bg-white h-1 w-[80px] rounded-md"></div>
+      <div className="bg-primary h-2 w-2 rounded-full"></div>
+      <div className="bg-white h-1 w-[80px] rounded-md "></div>
+      <div className="bg-primary h-2 w-2 rounded-full"></div>
     </div>
   )
 }
