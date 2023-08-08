d52ff80ea66fe163c936890c412a40315e6c2d9e
taugitirana <thayna@pharosit.com.br>
2023-08-08 18:05:03 -0300

    feat: create sign up multistep

--- src/app/components/steps.tsx
+++ src/app/components/steps.tsx
@@ -1,15 +1,58 @@
-export function Steps() {
+interface IStepProps {
+  step: 'step-1' | 'step-2' | 'step-3'
+}
+
+export function Steps({ step }: IStepProps) {
+  console.log(step)
   return (
     <div className="flex items-center justify-center gap-4 w-full">
       <div className="relative">
-        <div className="border-1 border-primary h-8 w-8 rounded-full flex flex-row justify-center items-center">
+        <div
+          className={
+            step === 'step-1'
+              ? 'border-1 border-primary h-8 w-8 rounded-full flex flex-row justify-center items-center'
+              : 'bg-primary h-2 w-2 rounded-full'
+          }
+        >
           <div className="bg-primary h-2 w-2 rounded-full"></div>
         </div>
       </div>
-      <div className="bg-white h-1 w-[80px] rounded-md"></div>
-      <div className="bg-primary h-2 w-2 rounded-full"></div>
-      <div className="bg-white h-1 w-[80px] rounded-md "></div>
-      <div className="bg-primary h-2 w-2 rounded-full"></div>
+      <div
+        className={
+          step !== 'step-1'
+            ? 'bg-primary h-1 w-[80px] rounded-md'
+            : 'bg-white h-1 w-[80px] rounded-md'
+        }
+      ></div>
+      <div className="relative">
+        <div
+          className={
+            step === 'step-2'
+              ? 'border-1 border-primary h-8 w-8 rounded-full flex flex-row justify-center items-center'
+              : 'bg-primary h-2 w-2 rounded-full'
+          }
+        >
+          <div className="bg-primary h-2 w-2 rounded-full"></div>
+        </div>
+      </div>
+      <div
+        className={
+          step === 'step-3'
+            ? 'bg-primary h-1 w-[80px] rounded-md'
+            : 'bg-white h-1 w-[80px] rounded-md'
+        }
+      ></div>
+      <div className="relative">
+        <div
+          className={
+            step === 'step-3'
+              ? 'border-1 border-primary h-8 w-8 rounded-full flex flex-row justify-center items-center'
+              : 'bg-primary h-2 w-2 rounded-full'
+          }
+        >
+          <div className="bg-primary h-2 w-2 rounded-full"></div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,55 +1,184 @@
 'use client'
 
 import { Button, Input, Image } from '@nextui-org/react'
-import { Fingerprint, AtSign, LogIn, Lock, ArrowRight } from 'lucide-react'
+import { Fingerprint, AtSign, Lock, ArrowRight } from 'lucide-react'
 import logo from '../assets/logo.png'
 import Link from 'next/link'
 import { Steps } from '../components/steps'
+import { useState } from 'react'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 export default function SignUp() {
+  const [step, setStep] = useState<'step-1' | 'step-2' | 'step-3'>('step-1')
+
+  const changeStep = () => {
+    switch (step) {
+      case 'step-1':
+        setStep('step-2')
+        break
+      case 'step-2':
+        setStep('step-3')
+    }
+  }
+
+  const signUpFormSchema = z.object({
+    registrationNumber: z
+      .string()
+      .toUpperCase()
+      .min(1, 'Type your registration number'),
+    email: z.string().email({
+      message: 'Type a valid e-mail',
+    }),
+    password: z.string().min(8, 'Type a valid password'),
+    confirmPassword: z.string().min(8, 'Type a valid password'),
+  })
+
+  type TSignUpData = z.infer<typeof signUpFormSchema>
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<TSignUpData>({
+    resolver: zodResolver(signUpFormSchema),
+  })
+
+  const handleSignUpSubmit: SubmitHandler<TSignUpData> = (
+    data: TSignUpData,
+  ) => {
+    const userSignUpData: TSignUpData = {
+      email: data.email,
+      password: data.password,
+      registrationNumber: data.registrationNumber,
+      confirmPassword: data.confirmPassword,
+    }
+
+    reset()
+  }
+
   return (
     <div className="grid grid-cols-2 min-h-screen">
       <div className="bg-primary"></div>
       <div className="bg-neutral-lighter flex flex-col items-center justify-center p-6">
-        <div className="max-w-sm w-full flex flex-col items-center gap-14">
-          <Steps />
+        <form
+          className="max-w-sm w-full flex flex-col items-center gap-14"
+          onSubmit={handleSubmit(handleSignUpSubmit)}
+        >
+          <Steps step={step} />
 
-          <Image width={130} src={logo.src} alt="" loading="lazy" />
+          <Image
+            width={130}
+            src={logo.src}
+            alt=""
+            loading="lazy"
+            className="rounded-none"
+          />
 
           <div className="flex flex-col gap-6 w-full">
-            <Input
-              label="Registration number"
-              isRequired
-              classNames={{
-                label: 'text-neutral',
-                input: ['bg-white', 'text-neutral-dark'],
-              }}
-              startContent={
-                <Fingerprint
-                  className="text-neutral"
-                  strokeWidth={1.5}
-                  size={20}
+            {step === 'step-1' ? (
+              <>
+                <Input
+                  label="Registration number"
+                  id="registrationNumber"
+                  classNames={{
+                    label: 'text-neutral',
+                    input: ['bg-white', 'text-neutral-dark'],
+                  }}
+                  startContent={
+                    <Fingerprint
+                      className={
+                        errors.registrationNumber
+                          ? 'text-error'
+                          : 'text-neutral'
+                      }
+                      strokeWidth={1.5}
+                      size={20}
+                    />
+                  }
+                  errorMessage={errors.registrationNumber?.message}
+                  validationState={errors.registrationNumber && 'invalid'}
+                  {...register('registrationNumber')}
                 />
-              }
-            />
 
-            <Input
-              label="Email"
-              isRequired
-              classNames={{
-                label: 'text-neutral',
-                input: ['bg-white', 'text-neutral-dark'],
-              }}
-              startContent={
-                <AtSign className="text-neutral" strokeWidth={1.5} size={20} />
-              }
-            />
+                <Input
+                  label="Email"
+                  isRequired
+                  classNames={{
+                    label: 'text-neutral',
+                    input: ['bg-white', 'text-neutral-dark'],
+                  }}
+                  startContent={
+                    <AtSign
+                      className="text-neutral"
+                      strokeWidth={1.5}
+                      size={20}
+                    />
+                  }
+                />
+              </>
+            ) : step === 'step-2' ? (
+              <>
+                <Input
+                  label="Password"
+                  isRequired
+                  classNames={{
+                    label: 'text-neutral',
+                    input: ['bg-white', 'text-neutral-dark'],
+                  }}
+                  startContent={
+                    <Lock
+                      className="text-neutral"
+                      strokeWidth={1.5}
+                      size={20}
+                    />
+                  }
+                />
+
+                <Input
+                  label="Confirm your password"
+                  isRequired
+                  classNames={{
+                    label: 'text-neutral',
+                    input: ['bg-white', 'text-neutral-dark'],
+                  }}
+                  startContent={
+                    <Lock
+                      className="text-neutral"
+                      strokeWidth={1.5}
+                      size={20}
+                    />
+                  }
+                />
+              </>
+            ) : (
+              <>
+                <div className="flex flex-col items-center text-center">
+                  <span className="text-md font-semibold text-neutral-dark">
+                    One last step!
+                  </span>
+                  <span className="text-sm text-neutral">
+                    In a few moments you are going to receive a verification
+                    email.
+                  </span>
+                  <span className="text-sm text-neutral">
+                    Please click on the link to finish your registration process
+                  </span>
+                </div>
+              </>
+            )}
           </div>
 
           <Button
-            className="bg-primary text-white w-full"
+            className={
+              step === 'step-3' ? 'hidden' : 'bg-primary text-white w-full'
+            }
             endContent={<ArrowRight size={20} />}
             radius="md"
+            onClick={changeStep}
+            type="submit"
           >
             Continue
           </Button>
@@ -57,7 +186,7 @@ export default function SignUp() {
           <Link href="/login" className="font-semibold text-primary text-sm">
             Back to Log In
           </Link>
-        </div>
+        </form>
       </div>
     </div>
   )
